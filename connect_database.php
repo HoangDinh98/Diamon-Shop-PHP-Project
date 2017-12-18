69ad281af78651b470e0da742451ffece5fb4ebe
PN\sinh.truong <sinh.truong@student.passerellesnumeriques.org>
2017-12-18 16:00:55 +0700

    Connect database

---
+++ connect_database.php
@@ -0,0 +1,9 @@
+<?php
+session_start(); // Starting Session
+$servername = "localhost";
+$db_user = "root";
+$db_password = "";
+$dbname = "diamond_shop";
+$connection = new mysqli($servername, $db_user, $db_password, $dbname);
+?>
+
