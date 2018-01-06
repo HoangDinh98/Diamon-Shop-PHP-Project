550beea0d091cd3bb1e2d1aa54f6a54a0c3b7bbc
MAYTINH-LA78A1G\MyPC <sinh.truong@student.passerellesnumeriques.org>
2018-01-06 19:41:11 +0700

    last version

--- Login/login.php
+++ Login/login.php
@@ -1,22 +1,45 @@
-<?php 
-session_start();
-include 'include.php';
-if(isset($_POST['submit']))
-{
-    $query = "SELECT * FROM users WHERE user_name = '".$_POST["username"]."' 
-        AND password ='".$_POST["password"]."'";
-    $result = mysqli_query($connect, $query);
-    if($row=mysql_fetch_array($result))
-    {
-        $_SESSION['username'] = $_POST['username'];
-        echo 'Yes';
-    }
- else {
-        echo 'No';
-    }
-}
-//if(isset($_POST["logout"]))
-//    {
-//    unset($_SESSION["username"]);
-//    }
+<?php
+//include '../include.php';
+include 'action_login.php';
 ?>
+
+<!DOCTYPE html>
+<html lang="vi">
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+        <link rel="stylesheet" href="../asset/bootstrap-3.3.7/css/bootstrap.min.css">
+        <script src="../asset/bootstrap-3.3.7/js/jquery-3.2.1.min.js"></script>
+        <script src="../asset/bootstrap-3.3.7/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="./style.css">
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-4 col-md-offset-4">
+                    <div class="account-wall">
+                        
+                        <img class="profile-img" src="https://lh5.googleusercontent.com/-b0-k99FZlyE/AAAAAAAAAAI/AAAAAAAAAAA/eu7opA4byxI/photo.jpg?sz=120"
+                             alt="img/jpg">
+                        
+                        <form class="form-signin" method="POST">
+                            <input type="text" class="form-control" placeholder="Email" name="username" id="username" value="<?php echo $user_name; ?>"  autofocus>
+                            <span class="error"><?php echo $user_nameErr;?></span>
+                            <input type="password" class="form-control" name="password" placeholder="Password" value="<?php echo $pass_word; ?>">
+                            <span class="error"><?php echo $pass_wordErr; ?></span>
+                            <button class="btn btn-lg btn-primary btn-block" type="submit" name="submit">
+                                Sign in</button>
+                            <label class="checkbox pull-left">
+                                <input type="checkbox" value="remember-me">
+                                Remember me
+                            </label>
+                            <a href="#" class="pull-right need-help">Need help? </a><span class="clearfix"></span>
+                        </form>
+                    </div>
+                    <a href="#" class="text-center new-account">Create an account </a>
+                </div>
+            </div>
+        </div>
+
+    </body>
+</html>
